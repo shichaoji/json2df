5e2c2f9cad5b098ddd4e6b52dd42cd7521fbbd2e
Shichao Ji <jshichao@vt.edu>
2017-12-30 20:07:01 -0500

    0.1.2

--- setup.py
+++ setup.py
@@ -2,14 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name = "json2df",
-    version = "0.1",
+    version = "0.1.2",
     description = "convert json data to Pandas DataFrame",
     author = "Shichao(Richard) Ji",
     author_email = "jshichao@vt.edu",
     url = "https://github.com/shichaoji2016/json2df",
     download_url = "https://github.com/shichaoji2016/json2df/archive/0.1.1.tar.gz",
+    license = 'MIT',
     keywords = ['data','json','dataframe','python'],
-    classifiers = ['Programming Language :: Python :: 2', 'Programming Language :: Python :: 3'],
+    classifiers = [
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 2', 
+	'Programming Language :: Python :: 3',
+	],
     packages = find_packages(),
+    install_requires=[
+          'pandas',
+      ]
 )
 
